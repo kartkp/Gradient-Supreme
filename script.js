08ae17d1fa011cd0824779d949cbd3aa9fee220d
Kartikey Pandey <122464491+kartkp@users.noreply.github.com>
2024-10-14 16:54:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hexNumbers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-let hex1 = ""
-let hex2 = ""
+let hex1 = "", hex2 = ""
+let prevHex1 = "", prevHex2 = ""
 
 document.querySelector("#generate").addEventListener("click", () => {
   generateRandomHex()
@@ -10,7 +10,13 @@ document.querySelector("#copy").addEventListener("click", () => {
   copyHex()
 })
 
+document.querySelector("#previous").addEventListener("click", () => {
+  applyPreviousGradient()
+})
+
 function generateRandomHex(){
+  prevHex1 = hex1
+  prevHex2 = hex2
   hex1 = "", hex2 = ""
   for(let i = 0; i < 6; i++){
     hex1 += hexNumbers[Math.floor(Math.random() * hexNumbers.length)]
@@ -24,4 +30,12 @@ function generateRandomHex(){
 function copyHex(){
   const gradient = `background: linear-gradient(45deg, #${hex1}, #${hex2})`
   navigator.clipboard.writeText(gradient)
+}
+
+function applyPreviousGradient(){
+  if (prevHex1 && prevHex2) {
+    document.querySelector("#hex1").innerHTML = prevHex1
+    document.querySelector("#hex2").innerHTML = prevHex2
+    document.body.style.background = `linear-gradient(45deg, #${prevHex1}, #${prevHex2})`
+  }
 }
